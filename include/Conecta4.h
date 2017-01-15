8e8bb051a7efa1dd6d75796bbb362ff45a237da3
Rafael <rafaelleru95103@gmail.com>
2017-01-15 21:58:14 +0100

    añadido contructor y metodo para evaluar

---
+++ include/Conecta4.h
@@ -0,0 +1,33 @@
+#ifndef __Conecta_4__
+#define __Conecta_4__
+
+#include "ArbolGeneral.h"
+#include "tablero.h"
+
+class Conecta4{
+ public:
+  //Almacena si un estado ha sido evaluado ya con la heuristica, su puntuacion y el tablero en si;
+  struct tritupla{
+    int puntuacion;
+    bool eval;
+    Tablero t;
+  }
+  
+  ArbolGeneral<tritupla> states;
+  int profundidad;
+  
+ private:
+  /**
+   * @brief crea una partida de Conecta4 vacia;
+   * @param p la profundidad hasta la que se explorara el arbol;
+   */
+  Conecta4(int p);
+
+  /**
+   *  \brief devuelve la evaluacion del Tablero contenido en t
+   *  \param t tritupla que contiene el tablero a evaluar;
+   *  \return puntuacion la puntuacion obtenida para el Tablero que contiene t
+   */
+  int eval(tritupla t);
+}
+#endif
