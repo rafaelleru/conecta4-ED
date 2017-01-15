68fa96c2f81d972d37800304cea1de112833773b
Rafael <rafaelleru95103@gmail.com>
2017-01-15 22:24:54 +0100

    initcpp

--- src/Conecta4.cpp
+++ src/Conecta4.cpp
@@ -1,3 +1,21 @@
 #include "Conecta4.h"
 
 using namespace std;
+
+int Conecta4::evalua(Conecta4::tritupla& t){
+  Tablero tab = t.t;
+  t.puntuacion = getHorizontales(t.t) + getVerticales(t.t) + getDiagonales(t.t);
+  return t.puntuacion;
+}
+
+
+
+
+
+//--------------------------------------------------------------------------------
+//Funciones de ayda que no pertenecen a la clase
+//--------------------------------------------------------------------------------
+
+int getHorizontales(Tablero& t){
+  vector<vector<int> > repr = t.getTablero();
+}
