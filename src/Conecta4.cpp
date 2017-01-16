6395c22313326539f7612498f965f59605544625
Rafael <rafaelleru95103@gmail.com>
2017-01-16 10:40:19 +0100

    implementado generaArbolEstads (Necesita ser revisado)

--- src/Conecta4.cpp
+++ src/Conecta4.cpp
@@ -5,10 +5,43 @@ using namespace std;
 int Conecta4::evalua(Conecta4::tritupla& t){
   Tablero tab = t.t;
   t.puntuacion = getHorizontales(t.t) + getVerticales(t.t) + getDiagonales(t.t);
-  return t.puntuacion;
+  return 10; //Cambiar aqui
+}
+
+void generaArbolEstados(int currentProf = 0, int prof = 2, ArbolGeneral<Tablero> t){
+  ArbolGeneral<Tablero>::nodo raiz = generados.getRaiz();
+  int nCol = t.getColumnas(); //Asignandolo aqui ahorramos multiples llamadas en el for.
+
+  //1ª version muy simple para generar el tablero.
+  //quiza se pueda paralelizar con posix y hebras
+  while(currentProf != prof){
+    bool hijomasizquierda = false; //Indica si hemos insertado el hijo mas a la izda o no
+
+    for(int i=0; i < nCol; ++i){
+      //Si se puede colocar la ficha, se llama recursivamente a generaArbolEstados para
+      //que se generen los hijos hasta la prof dada
+      if(t.colocarFicha(i)){
+	if(!hijomasizquierda){
+	  ArbolGeneral<Tablero>::nodo insertado(t);
+	  t.insertar_hijomasizquierda(insertado);
+	  ultimoInsertado = generados.hijomasizquierda;
+	  generaArbolEstados(currentProf+1, prof, ultimoInsertado);
+	  hijomasizquierda = true;
+	} else{
+	  ArbolGeneral<Tablero>::nodo insertado(t);
+	  t.insertar_hermanoderecha(ultimoInsertado, insertado);
+	  ultimoInsertado = ultimoInsertado.hermanoderecha();
+	  generaArbolEstados(currentProf+1, prof, ultimoInsertado);
+	}//else
+      }//if(t.colocarFicha(i))
+    }//for
+  }//while
+  
 }
 
 
+Tablero& Conecta4::siguienteMovimiento(ArbolGeneral<Tablero>& t){
+  
 
 
 
