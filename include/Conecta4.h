b3126e0ffb66973c1cd9b1c003db9967d3f579a6
Rafael <rafaelleru95103@gmail.com>
2017-01-16 10:39:59 +0100

    añadido generaArbolestados y siguiente movimiento

--- include/Conecta4.h
+++ include/Conecta4.h
@@ -12,10 +12,17 @@ class Conecta4{
     bool eval;
     Tablero t;
   }
-  
+
   ArbolGeneral<tritupla> states;
   int profundidad;
-  
+
+  /**
+   *  \brief genera los futuros estados para una profundida prof a partir del estado del tablero t
+   *
+   *  \param prof la profundidad que tendran los estados finales del arbol de estado (los nodos hoja)
+   *  \param t el tablero a partir del cual se ramifica para los futuros estados
+   */
+  int generaArbolEstados(int prof, Tablero t);			  
  private:
   /**
    * @brief crea una partida de Conecta4 vacia;
@@ -29,5 +36,15 @@ class Conecta4{
    *  \return puntuacion la puntuacion obtenida para el Tablero que contiene t
    */
   int evalua(tritupla& t);
+
+  /**
+   *  \brief obtiene el siguiente mejor movimiento
+   *
+   *  usamos anchura que garantiza la mejor solución.
+   *
+   *  \param n representa el estado a partir del cual jugamos
+   *  \return tablero quecontiene el siguiente movimiento.
+   */
+  Tablero& siguienteMovimiento(ArbolGeneral<Tablero> t);
 }
 #endif
