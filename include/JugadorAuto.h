1764c6905f5bcef82afda20fbad2726419c0e001
Rafael <rafaelleru95103@gmail.com>
2017-01-17 18:39:51 +0100

    añadido constructor y metodo para evaluar un tablero

--- include/JugadorAuto.h
+++ include/JugadorAuto.h
@@ -6,7 +6,31 @@
 using namespace std;
 
 class JugadorAuto{
+ private:
+  int turno;
+  int tope;
+
+  /**
+   *  \brief obtiene la heuristica para un nodo del arbol de estados dado
+   *
+   *  \param n el nodo que continee el tablero a evaluar
+   *  \return punt el valor obtenido por dicho nodo en base a nuestra heuristica.
+   */
+  int evaluaTablero(ArbolGeneral<Tablero>::Nodo& n);
  public:
+
+  /**
+   *  \brief constructor de jugador, se le pasa el turno y
+   *  opcinalmente la profundidad maxima de exploracion que realizara
+   *  sobre el arbol de estados del juego
+   *
+   *  \param turno el turno en el que juega el jugador \param tope el
+   *  maximo nivel de profundidad que se alcanzara explorando el arbol
+   *  de estados del juego, por defecto es 4.
+   */
+  JugadorAuto(int turno, int tope = 4);
+
+  
   /**
    * @brief Devuelve el nodo hijo del nodo raiz en el que está la mejor jugada
    * @param t ArbolGeneral<Tablero> Arbol de estados de los posibles tableros
