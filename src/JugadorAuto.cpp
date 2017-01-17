c3c7ac9fa6c9bc40e9e25779df7f1d25769a4e15
adriordi <adrihuelma22@gmail.com>
2017-01-17 18:41:33 +0100

    Done getNextBestMove v.1

--- src/JugadorAuto.cpp
+++ src/JugadorAuto.cpp
@@ -5,19 +5,28 @@ using namespace std;
 ArbolGeneral<Tablero>::Nodo& JugadorAuto::getNextBestMove(const ArbolGeneral<tablero>& t, int deep, int currentDeep){
 
     Nodo best_node;
-    int value_node, best_value_node;
+    int value_node = 0;
+    int best_value_node = -1;
+
     if(currentDeep <= deep){
+
       ArbolGeneral<Tablero>::preorden_iterador it = t.beginpreorden();
       for(it; it != t.endpreorden() && currentDeep != deep; ++it){
+
 	if(it.hermano() == 0)
 	  ++deep;
+
 	value_node = evalua(*it);
-	
-      }
+
+	if(value_node > best_value_node){
+	  best_value_node = value_node;
+	  best_node = *it;
+	}
+      }//for
     }
 
-    return best_node;
-  }//Fin metodo
+    for(deep; deep > currentDeep; --deep)
+      best_node = best_node->padre;
 
-  
-};//Fin clase
+    return best_node;
+}//metodo
